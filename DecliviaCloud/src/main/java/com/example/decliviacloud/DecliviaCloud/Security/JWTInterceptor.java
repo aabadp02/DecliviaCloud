bcd669a39d57f912d2dde33c50f951f90f801e55
aabadp02 <aabadp02@gmail.com>
2025-06-22 01:21:02 +0200

    Segunda prueba de subida

---
+++ DecliviaCloud/src/main/java/com/example/decliviacloud/DecliviaCloud/Security/JWTInterceptor.java
@@ -0,0 +1,17 @@
+package com.example.decliviacloud.DecliviaCloud.Security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class JWTInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
